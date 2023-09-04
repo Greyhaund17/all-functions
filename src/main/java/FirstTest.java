36435d6ec4222d42f8e0fad7a1f78c7d80ed1bce
Davit Sedrakyan <davosedrakya17n@gmail.com>
2023-09-04 22:28:44 +0400

    mangae timeout

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -4,10 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class FirstTest {
     @Test
     public void testSearchGoogle() {
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("http://google.com");
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("Call of Duty");
