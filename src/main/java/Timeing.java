6182432c738fa6ba7cfdb00a61d701bcbeb22df2
Davit Sedrakyan <davosedrakya17n@gmail.com>
2023-09-30 17:13:10 +0400

    timeingic heto gratsa vor vorosh elementner bacvum u pakvum en vorosh gortsoxutyunneric heto. aysinqn Add comment sexmum es heto pakum es (das 28 ete harcer linen)

--- src/main/java/Timeing.java
+++ src/main/java/Timeing.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,13 +12,23 @@ import java.util.concurrent.TimeUnit;
 
 public class Timeing {
     @Test
-    public void testSearchGoogle(){
-        WebDriver driver=new ChromeDriver();
-        driver.navigate().to("http://next.privat24.ua/");
-
-       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        WebElement wait=new WebDriverWait(driver,Duration.ofSeconds(5)).until(ExpectedConditions.
-                presenceOfElementLocated(By.xpath("//button[@data-qa-node='login']")));
-
-     }
+    public void testSearchGoogle() {
+        WebDriver driver = new ChromeDriver();
+        // driver.navigate().to("http://next.privat24.ua/");
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // WebElement wait=new WebDriverWait(driver,Duration.ofSeconds(5)).until(ExpectedConditions.
+        //         presenceOfElementLocated(By.xpath("//button[@data-qa-node='login']")));
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.navigate().to("https://next.privat24.ua/money-transfer/card?lang=en");
+        WebElement addComment = wait.until(ExpectedConditions.presenceOfElementLocated
+                (By.xpath("//span[@class='commentToggler_Jy6hnlSrFB']")));
+        addComment.click();
+        WebElement commentField = wait.until(ExpectedConditions.
+                presenceOfElementLocated(By.xpath("//textarea[@data-qa-node='comment']")));
+        WebElement closeElement = wait.until(ExpectedConditions.
+                presenceOfElementLocated(By.xpath("//div[@title='Close']")));
+        closeElement.click();
+        Assert.assertTrue(wait.until(ExpectedConditions.stalenessOf(commentField)));
+    }
 }
